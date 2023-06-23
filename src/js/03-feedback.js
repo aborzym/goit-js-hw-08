5745e0d7c07ebad42bfd3cf01dac3ff1925e4e85
aborzym <borzym.a@gmail.com>
2023-06-23 06:26:33 +0200

    all done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+'use strict';
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+
+const FORM_STATE = 'feedback-form-state';
+
+let formContent = {};
+form.addEventListener('input', throttle(updateFormState, 500));
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(FORM_STATE)));
+  localStorage.clear();
+  email.value = '';
+  message.value = '';
+}
+
+function updateFormState(event) {
+  formContent['email'] = email.value;
+  formContent['message'] = message.value;
+  localStorage.setItem(FORM_STATE, JSON.stringify(formContent));
+}
+
+getFormState();
+
+function getFormState(event) {
+  try {
+    const getState = JSON.parse(localStorage.getItem(FORM_STATE));
+    email.value = getState.email;
+    message.value = getState.message;
+    // getState.email ? (email.value = getState.email) : (email.value = '');
+    // getState.message
+    //   ? (message.value = getState.message)
+    //   : (message.value = '');
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
