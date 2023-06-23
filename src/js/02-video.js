cbd26e12cb12eb6d93d0a9dd7962a63bc888e7ad
aborzym <borzym.a@gmail.com>
2023-06-23 04:45:12 +0200

    video done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+'use strict';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime(time) {
+  localStorage.setItem(CURRENT_TIME, time.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
