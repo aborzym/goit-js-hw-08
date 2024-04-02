cd6747368a3fc540d388847ff2ee2ed1b6b6d6b1
aborzym <borzym.a@gmail.com>
2024-04-02 13:56:14 +0200

    task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery');
+
+const createGalleryItem = image => {
+  return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"> <img class="gallery__image" src="${image.preview}" alt="${image.description}" /></a></li>`;
+};
+
+const galleryString = galleryItems.map(createGalleryItem).join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryString);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
