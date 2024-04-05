2d88b213f9329f70aa2447e76fb21bfdd69cf74c
aborzym <borzym.a@gmail.com>
2024-04-05 16:49:03 +0200

    task 03 - partial

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form.feedback-form');
+const submitBtn = document.querySelector('button');
+
+const storedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (storedFormData) {
+  form.elements.email.value = storedFormData.email;
+  form.elements.message.value = storedFormData.message;
+}
+
+const delayedLocalStorageGetter = throttle((ev) => {
+  const formData = {
+    email: ev.currentTarget.elements.email.value,
+    message: ev.currentTarget.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 1000);
+
+form.addEventListener('input', delayedLocalStorageGetter);
+
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.clear();
+  form.reset();
+});
