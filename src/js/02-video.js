05958f9bc6d05bd503ff24a285c166ae5d3ca86d
aborzym <borzym.a@gmail.com>
2024-04-05 15:55:12 +0200

    task 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+const delayedTimeUpdate = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 999);
+
+player.on('timeupdate', data => {
+  delayedTimeUpdate(data);
+});
