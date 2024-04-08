c42571f299e9b019470ab76fc5b300be660a1924
aborzym <borzym.a@gmail.com>
2024-04-08 10:58:13 +0200

    update task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,13 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe#vimeo-player');
 const player = new Player(iframe);
+if (localStorage.key('videoplayer-current-time'))
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-const delayedTimeUpdate = throttle(data => {
+const delayedTimeUpdate = throttle((data) => {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }, 999);
 
-player.on('timeupdate', data => {
+player.on('timeupdate', (data) => {
   delayedTimeUpdate(data);
 });
